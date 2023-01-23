486455f038fd93ea6e41f47c282cec7fb29e4d71
Tima <Tima@LAPTOP-2U2N3SO0.mynetworksettings.com>
2023-01-22 20:05:20 -0500

    commit

---
+++ src/test/java/com/noorteck/qa/runners/Runner2.java
@@ -0,0 +1,18 @@
+package com.noorteck.qa.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@org.junit.runner.RunWith(Cucumber.class)
+
+@CucumberOptions(
+					features = "src/test/resources/hr/US205-GetAllDeps.feature",
+					glue = "com.noorteck.qa.steps",					
+							plugin = {"summary","pretty", "html:target/cucumber-reports.html",
+									"json:target/cucumber-reports",					
+									"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
+					monochrome = true
+						)
+public class Runner2 {
+
+}
